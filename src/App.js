048d57fa18ba4e1a71dbda52e00711ead4643102
Aishwarya Kumar <aishwaryakumar@Aishwaryas-MacBook-Pro.local>
2022-09-24 17:02:21 -0400

    added change to assignment 2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p1>
           Welcome to Web Development!
-        </p>
+        </p1>
+        <h2>
+          This is Branch A2.
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
