43126a2eee150070fb81dc5d30baceb579b25ab9
aishwaryakumara <aishwaryakumar.a4@gmail.com>
2022-11-20 23:58:36 -0500

    check

--- src/tuiter/tuit-list/index.js
+++ src/tuiter/tuit-list/index.js
@@ -10,6 +10,7 @@ const TuitList = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(findTuitsThunk())
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     console.log(tuits)
     console.log(loading)
